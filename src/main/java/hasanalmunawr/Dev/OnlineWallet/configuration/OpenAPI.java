2a00e2ad12ef37ef2d02daa4b08a8beebacfe963
hasanalmunawar <hasanalmunawar9@gmail.com>
2024-06-16 09:24:42 +0700

    Add OpenAPI

---
+++ src/main/java/hasanalmunawr/Dev/OnlineWallet/configuration/OpenAPI.java
@@ -0,0 +1,25 @@
+package hasanalmunawr.Dev.OnlineWallet.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Online Wallet Spring Boot",
+                version = "1.0.0",
+                description = "This Project an example implement backend side using spring boot",
+                termsOfService = "Example",
+                contact = @Contact(
+                        name = "hasan almunawar",
+                        email = "hasanalmunawar9@gmail.com"
+                ),
+                license = @License(
+                        name = "hasan to",
+                        url = "hasanalmunawar.com"
+                )
+        )
+)
+public class OpenAPI {
+}
